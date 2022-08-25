f921c185da460f54d306f0030a96d5ddd5f0f854
George Githae <gabs_githae@outlook.com>
2022-08-26 02:09:49 +0300

    #5 add core bundle

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,7 @@ return [
     // Add your bundles here with the associated env.
     // Ex:
     // Acme\Bundle\AppBundle\AcmeAppBundle::class => ['dev' => true, 'test' => true, 'prod' => true]
+    PcmtSharedBundle\PcmtSharedBundle::class => ['all' => true],
+    PcmtRulesBundle\PcmtRulesBundle::class => ['all' => true],
+    PcmtCISBundle\PcmtCISBundle::class => ['all' => true],
 ];
