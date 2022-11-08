414c62d96b429e7f665c3da71e8c3bfea69e2a83
George Githae <gabs_githae@outlook.com>
2022-11-08 23:05:21 +0300

    #17 added bundles PcmtPermissionsBundle PcmtPreReleaseBundle

--- config/bundles.php
+++ config/bundles.php
@@ -8,5 +8,7 @@ return [
     PcmtRulesBundle\PcmtRulesBundle::class => ['all' => true],
     PcmtCISBundle\PcmtCISBundle::class => ['all' => true],
     Pim\Bundle\CustomEntityBundle\PimCustomEntityBundle::class => ['all' => true],
-    PcmtDraftBundle\PcmtDraftBundle::class => ['all' => true]
+    PcmtDraftBundle\PcmtDraftBundle::class => ['all' => true],
+    PcmtPermissionsBundle\PcmtPermissionsBundle::class => ['all' => true],
+    PcmtPreReleaseBundle\PcmtPreReleaseBundle::class => ['all' => true]
 ];
