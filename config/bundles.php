acc2f712c06dada96ea93e36fd8a44144b3c42cd
George Githae <gabs_githae@outlook.com>
2022-11-29 13:40:44 +0300

    Refreshed repository with master branch from Akeneo

--- config/bundles.php
+++ config/bundles.php
@@ -5,10 +5,11 @@ return [
     // Ex:
     // Acme\Bundle\AppBundle\AcmeAppBundle::class => ['dev' => true, 'test' => true, 'prod' => true]
     PcmtSharedBundle\PcmtSharedBundle::class => ['all' => true],
-    PcmtRulesBundle\PcmtRulesBundle::class => ['all' => true],
-    PcmtCISBundle\PcmtCISBundle::class => ['all' => true],
-    Pim\Bundle\CustomEntityBundle\PimCustomEntityBundle::class => ['all' => true],
-    PcmtDraftBundle\PcmtDraftBundle::class => ['all' => true],
-    PcmtPermissionsBundle\PcmtPermissionsBundle::class => ['all' => true],
-    PcmtPreReleaseBundle\PcmtPreReleaseBundle::class => ['all' => true]
+    //PcmtRulesBundle\PcmtRulesBundle::class => ['all' => true],
+    //PcmtCISBundle\PcmtCISBundle::class => ['all' => true],
+    //Pim\Bundle\CustomEntityBundle\PimCustomEntityBundle::class => ['all' => true],
+    //PcmtDraftBundle\PcmtDraftBundle::class => ['all' => true],
+    //PcmtPermissionsBundle\PcmtPermissionsBundle::class => ['all' => true],
+    //PcmtPreReleaseBundle\PcmtPreReleaseBundle::class => ['all' => true],
+    //PcmtCoreBundle\PcmtCoreBundle::class => ['all' => true]
 ];
