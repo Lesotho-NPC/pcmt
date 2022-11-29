302db1176805fdb91fbe92adaaca77def26dd725
George Githae <gabs_githae@outlook.com>
2022-11-29 14:19:15 +0300

    Commented back the bundle that build successfully

--- config/bundles.php
+++ config/bundles.php
@@ -7,9 +7,9 @@ return [
     PcmtSharedBundle\PcmtSharedBundle::class => ['all' => true],
     //PcmtRulesBundle\PcmtRulesBundle::class => ['all' => true],
     //PcmtCISBundle\PcmtCISBundle::class => ['all' => true],
-    //Pim\Bundle\CustomEntityBundle\PimCustomEntityBundle::class => ['all' => true],
+    Pim\Bundle\CustomEntityBundle\PimCustomEntityBundle::class => ['all' => true],
     //PcmtDraftBundle\PcmtDraftBundle::class => ['all' => true],
     //PcmtPermissionsBundle\PcmtPermissionsBundle::class => ['all' => true],
-    //PcmtPreReleaseBundle\PcmtPreReleaseBundle::class => ['all' => true],
+    PcmtPreReleaseBundle\PcmtPreReleaseBundle::class => ['all' => true],
     //PcmtCoreBundle\PcmtCoreBundle::class => ['all' => true]
 ];
