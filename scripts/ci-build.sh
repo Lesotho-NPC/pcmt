4fe2d467db7fdbcd8dbc80e939136da4779275bf
george <gabs_githae@outlook.com>
2023-09-14 00:06:56 +0300

    #35 removed docker-compose up in ci-build.sh

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -11,11 +11,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd "$DIR/.."
 docker-compose build
-export DOCKER_CLIENT_TIMEOUT=1000
-export COMPOSE_HTTP_TIMEOUT=1000
-
-APP_ENV=dev docker-compose up -d
-docker/wait_docker_up.sh
-docker-compose exec -u www-data fpm sh -c "php bin/console pim:installer:db --catalog vendor/pcmt/custom-dataset-bundle/src/Resources/fixtures/pcmt_global"
 
 
