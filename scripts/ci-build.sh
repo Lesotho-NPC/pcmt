983683fcf30b9008efbb9a80b4e81ae48f246475
george <gabs_githae@outlook.com>
2023-09-14 13:23:23 +0300

    #35 reverted back to docker up

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -12,4 +12,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR/.."
 docker-compose build
 
+APP_ENV=dev docker-compose up -d
+docker/wait_docker_up.sh
+docker-compose exec -u www-data fpm sh -c "php bin/console pim:installer:db --catalog vendor/pcmt/custom-dataset-bundle/src/Resources/fixtures/pcmt_global"
+
 
