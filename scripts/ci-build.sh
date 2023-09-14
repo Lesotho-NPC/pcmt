86fe16317542cf31ecf0f339b67b18ab6a47fffd
george <gabs_githae@outlook.com>
2023-09-14 14:51:20 +0300

    #35 revert to build detached Increased timeout

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -10,6 +10,8 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd "$DIR/.."
+export DOCKER_CLIENT_TIMEOUT=120
+export COMPOSE_HTTP_TIMEOUT=120
 docker-compose build
 
 APP_ENV=dev docker-compose up -d
