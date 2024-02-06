d84bc79d7ab524f2fa725bffe49fba47d15637db
george <gabs_githae@outlook.com>
2024-02-06 21:41:31 +0300

    #51 added secrets on terraform

--- deploy/terraform/run-docker.sh
+++ deploy/terraform/run-docker.sh
@@ -57,8 +57,12 @@ cpFileFromEnvIntoHelper "$AWS_SHARED_CREDENTIALS_FILE" \
     "/tmp/.aws/aws-credentials"
 
 # copy deploy secrets into secrets volume
-cpFileFromEnvIntoHelper "$PCMT_SECRET_CONF" "/conf/parameters.yml.dist"
+cpFileFromEnvIntoHelper "$PCMT_SECRET_CONF" "/conf/parameters.env.dist"
 cpFileFromEnvIntoHelper "$PCMT_MYSQL_CREDS_CONF" "/conf/mysql-creds.env"
+cpFileFromEnvIntoHelper "$PCMT_S3_CREDS_CONF" "/conf/aws-s3-creds.env"
+cpFileFromEnvIntoHelper "$PCMT_FTP_GET_CREDS_CONF" "/conf/ftp-get-creds.env"
+cpFileFromEnvIntoHelper "$PCMT_FTP_PUT_CREDS_CONF" "/conf/ftp-put-creds.env"
+cpFileFromEnvIntoHelper "$PCMT_SFTP_PRIVKEY_FILENAME" "/conf/sftp-privkey"
 cpFileFromEnvIntoHelper "$PCMT_MYSQL_ROOT_PASSWORD_CONF" \
     "/conf/mysql-root-password.dist"
 cpFileFromEnvIntoHelper "$PCMT_MYSQL_USERNAME_CONF" \
