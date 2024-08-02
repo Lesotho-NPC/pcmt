de9e49d0dfafe7fcda4aa3d295abadb6429517aa
george <gabs_githae@outlook.com>
2024-08-02 20:33:56 +0300

    Added network on docker run

--- scripts/ci-behat.sh
+++ scripts/ci-behat.sh
@@ -19,7 +19,7 @@ echo "$0 run selenium ..."
 docker-compose -f docker-compose.dev.yml up -d selenium
 
 echo "$0 behat test for $PCMT_VER..."
-docker-compose -f docker-compose.dev.yml run -u www-data --rm php php /srv/pim/vendor/bin/behat
+docker-compose -f docker-compose.dev.yml run --network pim -u www-data --rm php php /srv/pim/vendor/bin/behat
 status=$?
 
 # Check if the tests failed
