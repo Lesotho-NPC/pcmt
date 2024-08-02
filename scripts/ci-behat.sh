27019d850f0d8deb09cf22c5cd4e5c2f7b028aea
george <gabs_githae@outlook.com>
2024-08-02 18:49:42 +0300

    Added behat test to staging env smart-test

---
+++ scripts/ci-behat.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+######################################################################
+# Copyright (c) 2023, VillageReach
+# Licensed under the Non-Profit Open Software License version 3.0.
+# SPDX-License-Identifier: NPOSL-3.0
+######################################################################
+
+# Path of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+PCMT_VER=$($DIR/pcmt-ver-sha.sh)
+
+cd "$DIR/.."
+
+echo "$0 install dependencies..."
+docker-compose -f docker-compose.dev.yml run -u www-data --rm php php -d memory_limit=4G /usr/local/bin/composer install
+
+echo "$0 behat test for $PCMT_VER..."
+docker-compose -f docker-compose.dev.yml run -u www-data --rm php php /srv/pim/vendor/bin/behat
+status=$?
+
+# Check if the tests failed
+if [ $status -ne 0 ]; then
+  echo "Tests failed with exit code $status"
+  exit $status
+fi
+
+echo "Tests completed."
