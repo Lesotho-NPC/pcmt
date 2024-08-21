ea86cf3903e7788bf6c36546d5be928746a0f814
Abdulkerim <abduljemal43@gmail.com>
2024-08-21 19:54:26 +0300

    fix docker profile

--- deploy/terraform/run-docker.sh
+++ deploy/terraform/run-docker.sh
@@ -83,6 +83,7 @@ docker run --rm \
     -e PCMT_PROFILE \
     -e PCMT_VER \
     -e PCMT_ASSET_URL \
+    -e DOCKER_PROFILES \
     -e AZURE_STORAGE_KEY \
     -e AZURE_STORAGE_ACCOUNT \
     -e PCMT_SECRETS_VOLUME="$SECRETS_VOL" \
