ba3730d9eb8edf6daa8477322be3e5db173cb3c3
Abdulkerim Jemal <abduljemal43@gmail.com>
2024-09-20 20:27:35 +0000

    #86, sso infrastructure setup

---
+++ deploy/terraform/env/modules/sso-azure/variables.tf
@@ -0,0 +1,35 @@
+variable "resource_group" {
+  description = "Resource Group the resources will reside"
+  type = string
+}
+
+variable "ssh_key_name" {
+  description = "Name of SSH Key hosted in Azure to place in VM"
+  type = string
+}
+
+variable "subnet_name" {
+  description = "Name of the subnet to attach instance to"
+  type = string
+}
+
+variable "network_name" {
+  description = "Name of the network the subnet belongs to"
+  type = string
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS Region to use"
+}
+
+
+variable "domain_name" {
+  type        = string
+  description = "AWS Route53 SSO Domain Name"
+}
+
+variable "route53_zone_name" {
+  description = "Name of zone to insert DNS record into"
+  type        = string
+}
