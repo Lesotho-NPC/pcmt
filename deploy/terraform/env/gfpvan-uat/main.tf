b9059a3da38d230cb00d404777f38bca809135a1
gabs.githae1 <gabs_githae@outlook.com>
2025-08-26 15:11:49 +0300

    #108 gfpvan moved aws to main, added missing variable instance-state

--- deploy/terraform/env/gfpvan-uat/main.tf
+++ deploy/terraform/env/gfpvan-uat/main.tf
@@ -3,6 +3,34 @@
 # Licensed under the Non-Profit Open Software License version 3.0.
 # SPDX-License-Identifier: NPOSL-3.0
 ######################################################################
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "s3" {
+    profile = "villagereach-gfpvan"
+    bucket  = "vr-gfpvan-terraform-states"
+    key     = "pcmt-gfpvan-uat.tf"
+    region  = "us-east-2"
+  }
+}
+
+provider "aws" {
+  alias   = "villagereach"
+  profile = "villagereach"
+  region  = var.aws-region
+}
+
+provider "aws" {
+  alias   = "gfpvan"
+  profile = "villagereach-gfpvan"
+  region  = var.aws-region
+}
 
 data "terraform_remote_state" "pcmt-network" {
   backend = "s3"
@@ -38,6 +66,7 @@ module "gfpvan-uat" {
   tag-bill-to       = var.tag-bill-to
   root-volume-size  = var.root-volume-size
   instance-type     = var.instance-type
+  instance-state    = var.instance-state
   app-deploy-group  = var.app-deploy-group
   domain-name       = var.domain-name
   subnet-id         = data.terraform_remote_state.pcmt-network.outputs.vpc-subnet-id
